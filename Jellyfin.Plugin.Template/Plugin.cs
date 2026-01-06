e4d147a77c4da97ef5cc1d3459588bf28eda4247
HovigCreates <hovigcc@gmail.com>
2026-01-06 02:03:10 +0000

    Prepare v0.9.0 release - Add metadata, update README and build config
    - Add project metadata to .csproj (Authors, Company, Description, Version, URLs)
    - Replace template README with plugin-specific documentation
    - Update build.yaml with correct plugin information
    - Update Plugin.cs and configuration files for password reset functionality

--- Jellyfin.Plugin.Template/Plugin.cs
+++ Jellyfin.Plugin.Template/Plugin.cs
@@ -1,51 +1,53 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using Jellyfin.Plugin.Template.Configuration;
+using Jellyfin.Plugin.UserPasswordReset.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
-namespace Jellyfin.Plugin.Template;
-
-/// <summary>
-/// The main plugin.
-/// </summary>
-public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+namespace Jellyfin.Plugin.UserPasswordReset
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="Plugin"/> class.
+    /// Jellyfin plugin that provides a configuration page.
     /// </summary>
-    /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
-    /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
-    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
-        : base(applicationPaths, xmlSerializer)
+    public sealed class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        Instance = this;
-    }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Plugin"/> class.
+        /// </summary>
+        /// <param name="applicationPaths">Application paths.</param>
+        /// <param name="xmlSerializer">XML serializer.</param>
+        public Plugin(
+            IApplicationPaths applicationPaths,
+            IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+        }
 
-    /// <inheritdoc />
-    public override string Name => "Template";
+        /// <inheritdoc />
+        public override string Name => "User Password Reset";
 
-    /// <inheritdoc />
-    public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+        /// <inheritdoc />
+        public override string Description =>
+            "Provides an admin configuration page.";
 
-    /// <summary>
-    /// Gets the current plugin instance.
-    /// </summary>
-    public static Plugin? Instance { get; private set; }
+        /// <inheritdoc />
+        public override Guid Id =>
+            Guid.Parse("d8c01d3b-9b3f-4f4c-a0f3-8d3a7a8b4c11");
 
-    /// <inheritdoc />
-    public IEnumerable<PluginPageInfo> GetPages()
-    {
-        return
-        [
-            new PluginPageInfo
+        /// <inheritdoc />
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
             {
-                Name = Name,
-                EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configPage.html", GetType().Namespace)
-            }
-        ];
+                new PluginPageInfo
+                {
+                    Name = "User Password Reset",
+                    EmbeddedResourcePath =
+                        "Jellyfin.Plugin.UserPasswordReset.Configuration.configPage.html"
+                }
+            };
+        }
     }
 }
